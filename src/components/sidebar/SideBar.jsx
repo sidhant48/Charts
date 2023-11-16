1af7d9400f6974a4d5a908893939a5c41c15e80e
sidhant48 <sidhant.b48@gmail.com>
2023-11-16 13:02:32 +0530

    commit

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Sidebar.css";
+import "./SideBar.css";
 
 import { FaDollarSign } from "react-icons/fa";
 import { IoDocumentTextSharp } from "react-icons/io5";
